d41029440c9e1405f5c50a99757ba534cd9586ba
Esther Leytush <eleytush@gmail.com>
2016-06-04 19:15:37 -0400

    minor sweep through controllers and views to update from removing devise

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,6 +10,7 @@ class Project < ActiveRecord::Base
   has_many :tags, through: :tag_projects
 
   private
+  
   def set_expiration
     if !self.expiration
       self.expiration = Date.today + 7.days
