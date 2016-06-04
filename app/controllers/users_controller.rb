7460d77f1f589beea92faf05b38230d6be4417c6
Samantha Holmes <samanthavholmes@gmail.com>
2016-06-03 20:00:45 -0400

    Work on user show page and make edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,32 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if current_user == @user
+      if params[:role] == 'developer' || params[:role] == 'designer'
+        current_user.update_attributes(role: params[:role])
+        if current_user.update_attributes(user_params)
+          redirect_to user_path(current_user)
+        else
+          render :edit
+        end
+      else
+        render :edit
+      end
+    end
+  end
+
   private
 
   def set_user
     @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :bio)
   end
 
 end
